64f29d81587df0f3af17604541598677de45e91c
Brandon O'Connor <boconnor@tableau.com>
2016-02-11 18:14:45 -0800

    initial code commit

--- README.md
+++ README.md
@@ -1,26 +1,58 @@
 ## Synopsis
 
-At the top of the file there should be a short introduction and/ or overview that explains **what** the project is. This description should match descriptions added for package managers (Gemspec, package.json, etc.)
-
-## Code Example
-
-Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+This is a dead simple script that sorts a user's flickr photos into an album of the camera name it was taken by according to its EXIF data.
 
 ## Motivation
 
-A short description of the motivation behind the creation and maintenance of the project. This should explain **why** the project exists.
+This project was written because Flickr's organize interface has no option to sort and categorize photos by camera type. I wanted chromecast to cycle through only photos of high quality through nicer cameras and the only way to do this was by organizing photos into an album per camera source.
 
 ## Installation
 
-Provide code examples and explanations of how to get the project.
+```
+> mkvirtualenv flickr-album-per-camera
+> pip install -r requirements.txt
+> python cameras_to_albums.py --help
+usage: cameras_to_albums.py [-h] [-d] [-k API_KEY] [-s API_SECRET]
+                            [-u USERNAME]
 
-## API Reference
+optional arguments:
+  -h, --help            show this help message and exit
+  -d, --dry_run         Verbose minus action. Default=False
+  -k API_KEY, --api_key API_KEY
+                        flickr API key
+  -s API_SECRET, --api_secret API_SECRET
+                        flickr API secret
+  -u USERNAME, --username USERNAME
+                        your flickr username
+```
 
-Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+## Bugs / Development / Contributing
+* Report issues/questions/feature requests on in the [Issues](https://github.com/brandoconnor/flickr-album-per-camera/issues) section.
+* Feel free to ask questions via email.
 
-## Contributors
+Pull requests are welcome!
+Ideally create a topic branch for every separate change you make.
+For example:
 
-Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
+1. Fork the repo
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. If possible, write some tests.
+4. Commit your awesome changes (`git commit -am 'Added some feature'`)
+5. Push to the branch (`git push origin my-new-feature`)
+6. Create a new Pull Request and tell us about your changes.
+
+## LICENSE
+Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
+
+
 
 ## License
 
---
+++ cameras_to_albums.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""A simple script to organize photos into albums named by their camera source.
+For me, this is useful for cycling through only high quality photos on my TV
+hooked up to a chromecast."""
+
+import flickrapi
+import argparse
+
+
+def auth_flickr(api_key, api_secret):
+    """Authenticate user to flickr API."""
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
+    flickr.authenticate_via_browser(perms='write')
+    return flickr
+
+
+def get_user_id(flickr, user):
+    """Get the user_id from the username."""
+    user_data = flickr.people.findByUsername(username=user)
+    return user_data['user']['id']
+
+
+def get_photoset_dict(flickr, user_id):
+    """construct a photoset dict of album name to album/set id."""
+    print('Gathering dictionary of all photos in all photosets...')
+    init_photoset = flickr.photosets.getList(user_id=user_id)
+    photoset_dict = dict()
+    for set_page in range(1, init_photoset['photosets']['pages'] + 1):
+        photoset = flickr.photosets.getList(user_id=user_id, page=set_page)
+        for pset in photoset['photosets']['photoset']:
+            # a_dict = {'thing': {'this': ['value', 'list']} }
+            photoset_dict[pset['title']['_content']] = {pset['id']: []}
+            init_photoset_object = flickr.photosets.getPhotos(photoset_id=pset['id'])
+            for photoset_page in range(1, init_photoset_object['photoset']['pages'] + 1):
+                photoset_object = flickr.photosets.getPhotos(user_id=user_id, photoset_id=pset['id'], page=photoset_page)
+                photoset_dict[pset['title']['_content']][pset['id']] += [p['id'] for p in photoset_object['photoset']['photo']]
+    return photoset_dict
+
+
+def main(args):
+    """Main code block. Do all the things."""
+    flickr = auth_flickr(args.api_key, args.api_secret)
+    user_id = get_user_id(flickr, user=args.username)
+    album_dict = get_photoset_dict(flickr, user_id)
+    init_photos = flickr.people.getPhotos(user_id=user_id)
+    total = init_photos['photos']['total']
+    photo_num = 0
+    for page_num in range(1, init_photos['photos']['pages'] + 1):
+        photo_batch = flickr.people.getPhotos(user_id=user_id, page=page_num)
+        for photo in photo_batch['photos']['photo']:
+            photo_num += 1
+            photo_id = photo['id']
+            print('processing photo %s of %s: %s' % (photo_num, total, photo_id))
+            photo_data = flickr.photos.getExif(photo_id=photo_id)
+            camera_name = photo_data['photo']['camera']
+            if len(camera_name) > 0:
+                if camera_name not in album_dict.keys():
+                    print('adding camera album "%s"' % camera_name)
+                    new_set = flickr.photosets.create(title=camera_name,
+                                                      primary_photo_id=photo_id,
+                                                      description='All photos taken behind a %s' % camera_name)
+                    album_dict[camera_name] = new_set['photoset']['id']
+                    continue
+                elif photo_id not in [p for p in album_dict[camera_name].values()[0]]:
+                    # if this photo is not in the appropriate album, add it
+                    print('Adding photo to camera album.')
+                    flickr.photosets.addPhoto(photoset_id=album_dict[camera_name].keys()[0], photo_id=photo_id)
+                else:
+                    print('Photo is already in the appropriate set')
+                    continue
+            else:
+                print('Skipping photo with insufficient metadata.')
+
+
+if __name__ in '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dry_run', action='store_true',
+                        default=False, help="Verbose minus action. Default=False")
+    parser.add_argument('-k', '--api_key', help='flickr API key')
+    parser.add_argument('-s', '--api_secret', help='flickr API secret')
+    parser.add_argument('-u', '--username', help='your flickr username')
+    exit(main(parser.parse_args()))
